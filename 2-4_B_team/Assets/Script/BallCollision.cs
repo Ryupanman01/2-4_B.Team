fb7c8072ff724d4fa37de086cf89d622d32e88dc
KBC12006160\kbc12006160 <ryu-ookouchi_20h@stu.kbc.ac.jp>
2021-10-18 14:57:40 +0900

    ScoreCounterスクリプトの追加、スコアテキストの追加、ボールに触れたアイテムを消す

--- 2-4_B_team/Assets/Script/BallCollision.cs
+++ 2-4_B_team/Assets/Script/BallCollision.cs
@@ -7,7 +7,7 @@ public class BallCollision : MonoBehaviour
     void OnCollisionEnter(Collision collision)
     {
         //衝突した相手にPlayerタグがついているとき
-        if(collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
             //0.2秒後に消える
             Destroy(gameObject, 0.2f);
---
+++ 2-4_B_team/Assets/Script/ScoreCounter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCounter : MonoBehaviour
+{
+    public Text ScoreText; //Text用変数
+    private int score;          //スコア計算用変数初期化
+
+    void Start()
+    {
+        Initialize();
+    }
+    private void Initialize()
+    {
+        score = 1;
+        ScoreText.text = "Score:" + score;
+    }
+    public void AddScore()
+    {
+        score += 1;
+        ScoreText.text = "Score:" + score;
+    }
+}
