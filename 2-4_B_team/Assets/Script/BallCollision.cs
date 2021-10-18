b1b5ee695a1456f481d412224de0e087ecc77db2
KBC12006160\kbc12006160 <ryu-ookouchi_20h@stu.kbc.ac.jp>
2021-10-18 17:06:51 +0900

    アイテムに触れたらカウントする機能追加

--- 2-4_B_team/Assets/Script/BallCollision.cs
+++ 2-4_B_team/Assets/Script/BallCollision.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class BallCollision : MonoBehaviour
 {
+    GameObject ScoreObject;
+
+    void Start()
+    {
+        ScoreObject = GameObject.Find("ScoreCounter");    
+    }
     void OnCollisionEnter(Collision collision)
     {
         //衝突した相手にPlayerタグがついているとき
         if (collision.gameObject.tag == "Player")
         {
+            ScoreObject.GetComponent<ScoreCounter>().AddScore();
             //0.2秒後に消える
             Destroy(gameObject, 0.2f);
         }
--- 2-4_B_team/Assets/Script/ScoreCounter.cs
+++ 2-4_B_team/Assets/Script/ScoreCounter.cs
@@ -5,21 +5,16 @@ using UnityEngine.UI;
 
 public class ScoreCounter : MonoBehaviour
 {
-    public Text ScoreText; //Text用変数
-    private int score;          //スコア計算用変数初期化
+    int score = 0;
+    public Text ScoreText;
 
-    void Start()
-    {
-        Initialize();
-    }
-    private void Initialize()
-    {
-        score = 1;
-        ScoreText.text = "Score:" + score;
-    }
     public void AddScore()
     {
-        score += 1;
-        ScoreText.text = "Score:" + score;
+        score++;
+    }
+
+    void Update()
+    {
+        ScoreText.text = "Count:" + score.ToString();
     }
 }
